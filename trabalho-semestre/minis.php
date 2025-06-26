4459baff2b4f570276d72290f7562ef773f3f6b2
flavio-a-galvao <flavioaugusto.galvao@gmail.com>
2025-06-26 16:02:27 -0300

    arrumando animação da ampliação da imagem

--- trabalho-semestre/minis.php
+++ trabalho-semestre/minis.php
@@ -10,7 +10,7 @@ include 'produtos.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Leila Pães e Salgados</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css?v=3">
   <link rel="stylesheet" href="css/aos.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
