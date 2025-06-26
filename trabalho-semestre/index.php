95e750832ae37e204acc908e8524172863b5c8d5
flavio-a-galvao <flavioaugusto.galvao@gmail.com>
2025-06-26 08:48:57 -0300

    adicionando animação

--- trabalho-semestre/index.php
+++ trabalho-semestre/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Leila Pães e Salgados</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -131,7 +131,7 @@
 
 <footer class="footer">
     <div class="footer-content">
-        <p>Estamos sempre prontos para atender você!</p>
+        <p>&copy; 2025 Meu Site. Todos os direitos reservados.</p>
         <a href="https://wa.me/5544998086959" class="whatsapp-icon" target="_blank">
             <i class="fab fa-whatsapp"></i>
         </a>
@@ -139,5 +139,10 @@
 </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  <script src="js/jquery-1.11.3.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 </body>
 </html>
