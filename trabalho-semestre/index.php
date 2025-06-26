18eff5b482c51d095bf1268ba9e4b028dbab3f23
flavio-a-galvao <flavioaugusto.galvao@gmail.com>
2025-06-26 08:56:33 -0300

    adicionando animação e fonte

--- trabalho-semestre/index.php
+++ trabalho-semestre/index.php
@@ -5,8 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Leila Pães e Salgados</title>
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/aos.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
   <nav class="navbar bg-body-tertiary">
@@ -49,18 +51,18 @@
 
   <img class="wallpaper" src="imagens/wallpaper2.jpg" alt="Descrição da imagem" width="100%">
 
-  <h2 class="text-center">Entre em contato com a gente</h2>
+  <h2 class="text-center" data-aos="flip-left">Entre em contato com a gente</h2>
 
-  <div class="container">
+  <div class="container" data-aos="flip-left">
     <a href="https://wa.me/5544998086959" class="whatsapp-button" target="_blank">
       <i class="fab fa-whatsapp"></i> Entre em Contato
     </a>
   </div>
 
-  <h2 class="text-center">Nosso cardapio</h2>
+  <h2 class="text-center" data-aos="flip-left">Nosso cardapio</h2>
 
  <div class="card-container">
-        <div class="card">
+        <div class="card" data-aos="flip-left">
             <img src="https://via.placeholder.com/150" class="card-img-top" alt="Imagem do Card">
             <div class="card-body">
                 <h5 class="card-title">Card 1</h5>
@@ -73,7 +75,7 @@
                 </div>
             </div>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-left">
             <img src="https://via.placeholder.com/150" class="card-img-top" alt="Imagem do Card">
             <div class="card-body">
                 <h5 class="card-title">Card 2</h5>
@@ -86,7 +88,7 @@
                 </div>
             </div>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-left">
             <img src="https://via.placeholder.com/150" class="card-img-top" alt="Imagem do Card">
             <div class="card-body">
                 <h5 class="card-title">Card 3</h5>
@@ -99,7 +101,7 @@
                 </div>
             </div>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-left">
             <img src="https://via.placeholder.com/150" class="card-img-top" alt="Imagem do Card">
             <div class="card-body">
                 <h5 class="card-title">Card 4</h5>
@@ -112,7 +114,7 @@
                 </div>
             </div>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-left">
             <img src="https://via.placeholder.com/150" class="card-img-top" alt="Imagem do Card">
             <div class="card-body">
                 <h5 class="card-title">Card 5</h5>
