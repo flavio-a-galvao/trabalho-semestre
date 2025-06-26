5d5b6cda4413a77dba08369d6c9410f5bb1e4ba2
flavio-a-galvao <flavioaugusto.galvao@gmail.com>
2025-06-26 11:56:11 -0300

    tirando alguns erros

--- trabalho-semestre/produtos.php
+++ trabalho-semestre/produtos.php
@@ -2,61 +2,61 @@
 $salgadosGrandes = [
     [
         'nome' => 'Coxinha de Carne,
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00,
         'imagem' => 'imagens/coxinha-carne.png',
     ],
     [
         'nome' => 'Coxinha de Frango',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00,
         'imagem' => 'imagens/coxinha-de-frango.png',
     ],
     [
         'nome' => 'Kibi com queijo',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00
         'imagem' => 'imagens/kibi.png,
     ],
     [
         'nome' => 'Risoles de Carne',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00,
         'imagem' => 'imagens/risoles-carne.png',
     ],
     [
         'nome' => 'Risoles de Frango',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00,
         'imagem' => 'imagens/risoles-frango.png',
     ],
     [
         'nome' => 'Risoles de Presunto',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 7.00,
         'imagem' => 'imagens/risoles-de-presunto.png',
     ],
     [
         'nome' => 'Espetinho de Porco',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 8.00,
         'imagem' => 'imagens/espetinho.png',
     ],
     [
         'nome' => 'Espetinho de Boi',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 8.00,
         'imagem' => 'imagens/espetinho.png',
     ],
     [
         'nome' => 'Pastel de Presunto',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 8.00,
         'imagem' => 'imagens/pastel-presunto.png',
     ],
     [
         'nome' => 'Pastel de Carne',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 8.00,
         'imagem' => 'imagens/pastel-carne.png',
     ],
@@ -64,37 +64,37 @@ $salgadosGrandes = [
 $salgadoPequenos[
     [
         'nome' => 'Mini Bolinha de Queijo',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/mini-bolinha-queijo.png',
     ],
     [
         'nome' => 'Mini Coxinha de Carne',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/mini-coxinha-carne.png'
     ],
     [
         'nome' => 'Mini Coxinha de Frango',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/mini-coxinha-frango.png'
     ],
     [
         'nome' => 'Mini Traveseirinho de Carne',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/mini-traveseirinho-carne.png'
     ],
         [
         'nome' => 'Mini Traveseirinho de Presunto',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/mini-traveseirinho-presunto.png'
     ],
         [
         'nome' => 'Kibi com Queijo',
-        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card.',
+        'descricao' => 'Algum texto de exemplo para construir o conteúdo do card',
         'preco' => 0.90,
         'imagem' => 'imagens/kibi.png'
     ],
